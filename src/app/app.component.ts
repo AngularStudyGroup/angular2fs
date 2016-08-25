8e3af2728c1e5ca9b0edc441662dbe60c5315098
syhily <syhily@gmail.com>
2016-08-26 05:59:42 +0800

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles.css';
+
+@Component({
+    selector: 'my-app',
+    template: require('./app.component.html')
+})
+
+export class AppComponent {
+}
